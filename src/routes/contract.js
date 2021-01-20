ab1517a0629b46435d7ae1ae23a7b8b47c1e9bf6
Miura <tadamiura@gmail.com>
2021-01-20 18:15:24 +0100

    created a route to read all contracts

---
+++ src/routes/contract.js
@@ -0,0 +1,25 @@
+const express = require ('express')
+const { connection } =require('../helper/conf')
+
+const router = express.Router() 
+
+//Get list of contract of an user
+router.get('/', (req,res) => {
+    const sql = `
+    SELECT title, description, icon_url 
+    FROM contract 
+    JOIN contract_user 
+    ON contract.id = contract_user.contract_id
+    JOIN user
+    ON user.id = contract_user.user_id`
+    // const sql = `SELECT * FROM contract`
+    connection.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).send('Error to get the list of contracts')
+        } else {
+            res.send(result)
+        }
+    })
+})
+
+module.exports = router
